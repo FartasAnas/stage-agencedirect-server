b29430ea086f2a3999fd4628dc46c6cb28816d99
FartasAnas <fartasanas@gmail.com>
2022-07-21 09:08:08 +0100

    first commit

---
+++ src/main/java/stage/agencedirectserver/AgencedirectServerApplication.java
@@ -0,0 +1,13 @@
+package stage.agencedirectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencedirectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencedirectServerApplication.class, args);
+	}
+
+}
